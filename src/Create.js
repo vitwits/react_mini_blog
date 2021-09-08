8f4281b9d6eac14af81f1ade7960bc340f1a50d0
vitwits <vitwits@gmail.com>
2021-09-08 08:38:41 +0300

    redirecting to home page with useHistory hook

--- src/Create.js
+++ src/Create.js
@@ -1,40 +1,66 @@
 import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 
 const Create = () => {
   const [title, setTitle] = useState( '' );
   const [body, setBody] = useState( '' );
   const [author, setAuthor] = useState( 'mario' );
+  const [isPending, setIsPending] = useState(false);
+  const history = useHistory()
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+
+    setIsPending(true);
+
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs', {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify(blog)
+      }).then(() => {
+        console.log('new blog added');
+        setIsPending(false);
+        // history.go(-1); // go to previous page
+        history.push('/'); //redirecting to home page
+      })
+    }, 1000);
+
+  }
 
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>Blog title:</label>
         <input
           type="text"
           required
           value={title}
-          onChange={(e) => setTitle(e.target.value)}
+          onChange={(e) => setTitle( e.target.value )}
         />
-        <label>Blog title:</label>
+        <label>Blog body:</label>
         <textarea
           required
           value={body}
-          onChange={(e) => setBody(e.target.value)}
+          onChange={(e) => setBody( e.target.value )}
         >
         </textarea>
         <label>Blog author</label>
         <select
           value={author}
-          onChange={(e) => setAuthor(e.target.value) }
+          onChange={(e) => setAuthor( e.target.value )}
         >
           <option value="mario">mario</option>
           <option value="yoshi">yoshi</option>
         </select>
-        <button>Add Blog</button>
+        {!isPending && <button>Add Blog</button>}
+        {isPending && <button disabled>Adding Blog...</button>}
       </form>
     </div>
   );
 };
 
 export default Create;
+
